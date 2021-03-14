7c138cd01f9e36575953e207dd98a8752efdd257
DANIEL GERARDO ZAMORA SALAS <701430017@castrocarazo.ac.cr>
2021-03-14 16:35:53 -0600

    Controlador Login

---
+++ SistemaLavado/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SistemaLavado.Models;
+
+namespace SistemaLavado.Controllers
+{
+    public class LoginController : Controller
+    {
+        sistemadecontrolEntities BD_Login = new sistemadecontrolEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()           
+        {
+            var registro = BD_Login.pa_UsuariosSelect(null).ToList();
+            return View(registro);
+        }
+    }
+}
