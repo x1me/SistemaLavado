ef608bb880a6d18ddfb82043e2a97d442bd7def2
XIMENA ESPINOZA CAMBRONERO <117440770@castrocarazo.ac.cr>
2021-03-14 15:54:20 -0600

    VistaListaFabricante

--- SistemaLavado/Controllers/MantenimientoController.cs
+++ SistemaLavado/Controllers/MantenimientoController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SistemaLavado.Models;
 
 namespace SistemaLavado.Controllers
 {
     public class MantenimientoController : Controller
     {
+        sistemadecontrolEntities ModeloBD = new sistemadecontrolEntities();
         // GET: Mantenimiento
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ListaFabricante()
+        {
+            List<pa_fabricanteSelect_Result> ModeloVista = this.ModeloBD.pa_fabricanteSelect(null,null).ToList();
+            return View(ModeloVista);
         }
     }
 }
