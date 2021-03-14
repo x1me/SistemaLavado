1f4c506e12f9e06bdb8729cb9927713c4b1ba1ff
XIMENA ESPINOZA CAMBRONERO <117440770@castrocarazo.ac.cr>
2021-03-14 15:40:07 -0600

    Trabajo

---
+++ SistemaLavado/Controllers/MantenimientoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaLavado.Controllers
+{
+    public class MantenimientoController : Controller
+    {
+        // GET: Mantenimiento
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
