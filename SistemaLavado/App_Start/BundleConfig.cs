fad4a2382469a159acd46bd02e4b2b78a525a283
DANIEL GERARDO ZAMORA SALAS <701430017@castrocarazo.ac.cr>
2021-03-21 22:23:39 -0600

    Pagina principal desplegable

--- SistemaLavado/App_Start/BundleConfig.cs
+++ SistemaLavado/App_Start/BundleConfig.cs
@@ -9,22 +9,13 @@ namespace SistemaLavado
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                        "~/Scripts/jquery-{version}.js", 
+                        "~/Scripts/jquery.validate*" , 
+                        "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/Site.css"));
         }
     }
 }
