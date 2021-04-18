1117a4d8c83ba7d4e834b9e717c7547d6e35a555
DANIEL GERARDO ZAMORA SALAS <701430017@castrocarazo.ac.cr>
2021-04-18 12:58:52 -0600

    vehiculo

---
+++ SistemaLavado/Controllers/VehiculosController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SistemaLavado.Models;
+namespace SistemaLavado.Controllers
+{
+    public class VehiculosController : Controller
+    {
+        // GET: Vehiculos
+        sistemacontrolEntities bd = new sistemacontrolEntities();
+        public ActionResult Index()
+        {
+            string tipoUsuario = Session["role"] as string;
+            int? idCliente = Session["idCliente"] as Nullable<int>;
+            ViewBag.tipo = Session["role"] as string;
+            return View("ListaVehiculo");
+        }
+        [HttpGet, ActionName("listar")]
+        public ActionResult ListaVehiculo()
+        { // crear pa vehiculo y cambiar id por nombres
+            var Vehiculos = bd.pa_v().ToList();
+            var datos = (from i in Marca
+                         select new
+                         {
+                             tipo = bd.TipoVehiculo.Where(e => e.id_codigoTV == i.tipo).Select(e => e.tipo).First(),
+                             id_codigoMarcaV = i.id_codigoMarcaV,
+                             codigo = i.codigo,
+                             fabricante = bd.Fabricante.Where(e => e.id_codfabricante == i.fabricante).Select(e => e.pais).First(),
+                         }
+                        ).ToList();
+            return Json(datos, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ SistemaLavado/Scripts/Validaciones/Vehiculo.js
@@ -0,0 +1,105 @@
+﻿$(function () {
+    crearTabla();
+})
+
+function peticion(url, callback = null, metodo = "get", dataType = "json", datos = null) {
+    ///invocar al método
+    $.ajax({
+        url: url,///dirección del método
+        method: metodo,
+        dataType: dataType,
+        ///formato en el que se envían y reciben los datos
+        data: datos,///parámetros convertidos en formato JSON
+        ///función que se ejecuta cuando la respuesta fue satisfactoria
+        ///data: contiene el valor retornado por el método del servidor
+    }).done(function (response) {
+        callback(response);
+    }).fail(function (response) {
+        console.log("fail");
+    });
+}
+
+
+function crearTabla(datos) {
+    console.log($("ok"));
+    $("#Lista").kendoGrid({
+        dataSource: datos,
+        height: 500,
+        filterable: true,
+        pageSize: 20,
+        groupable: true,
+        sortable: true,
+        editable: "inline",
+        toolbar: ["search"],
+        pageable: {
+            refresh: true,
+            pageSizes: true,
+            buttonCount: 5,
+        },
+        columns: [
+            {
+                field: "id_vehiculo",
+                title: " ID Vehículo"
+            },
+            {
+                field: "placa",
+                title: "Placa"
+            },
+            {
+                field: "tipo",
+                title: "Tipo"
+            },
+            {
+                field: "marca",
+                title: "Marca"
+            },
+            {
+                field: "numeroPuertas",
+                title: "Cantidad Puertas"
+            },
+            {
+                field: "numeroRuedas",
+                title: "Cantidad Ruedas"
+            },
+            {
+                command: ["edit", "destroy"]
+            },
+        ],
+        dataSource: {
+            transport: {
+                read: {
+                    url: "/Vehiculo/listar",
+                },
+                update: {
+                    url: "/Vehiculo/agregaroeditar",
+                    type: "post",
+                    dataType: "json",
+                },
+                destroy: {
+                    url: "/Vehiculo/Elimina",
+
+                },
+            },
+            requestEnd: function (e) {
+                if ((e.type == "update" || e.type == "destroy") && e.response) {
+                    $("#Lista").data("kendoGrid").dataSource.read();
+                    $("#Lista").data("kendoGrid").refresh();
+                    alert(e.response);
+                }
+            },
+            schema: {
+                model: {
+                    id: "id_vehiculo",
+                    fields: {
+                        id_vehiculo: { editable: false, nullable: true },
+                        placa: { validation: { required: true, minlength: 1, maxlength: 30 } },
+                        tipo: { validation: { required: true, minlength: 1, maxlength: 30 } },
+                        marca: { validation: { required: true, minlength: 1, maxlength: 30 } },
+                        numeroPuertas: { type: "number", validation: { required: true, min: 1, max: 1000 } },
+                        numeroRuedas: { type: "number", validation: { required: true, min: 1, max: 1000 } }
+                    }
+                }
+            }
+        },
+    })
+}
